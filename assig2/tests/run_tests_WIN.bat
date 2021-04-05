51162c8ce2cdcb8c6bc221040dbfd0510543ebd8
Dylan Olney <dylan.olney@mycit.ie>
2021-04-05 22:32:01 +0100

    minor change

--- assig2/tests/run_tests_WIN.bat
+++ assig2/tests/run_tests_WIN.bat
@@ -1,8 +1,8 @@
 @echo off
-echo "Running Postman test suite in Newman (from Docker Image)!"
-echo "Please wait..."
+echo Running Postman test suite in Newman (from Docker Image)!
+echo Please wait...
 docker run --net=host -v  "%~dp0/postman":/etc/newman -t postman/newman run collection.json 
-echo "Tests complete!"
+echo Tests complete!
 pause
 
 
